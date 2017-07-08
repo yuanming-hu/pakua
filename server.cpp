51b919960c058daa1039429921cbff221fc9e53f
Yuanming Hu <iterator.advance@gmail.com>
2017-07-08 19:41:21 -0400

    It runs

--- server.cpp
+++ server.cpp
@@ -1,10 +1,8 @@
+
 #define ASIO_STANDALONE
 
 #include <websocketpp/config/asio_no_tls.hpp>
-
 #include <websocketpp/server.hpp>
-
-#include <iostream>
 
 typedef websocketpp::server<websocketpp::config::asio> server;
 
@@ -16,7 +14,7 @@ using websocketpp::lib::bind;
 typedef server::message_ptr message_ptr;
 
 // Define a callback to handle incoming messages
-void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
+void on_message(server *s, websocketpp::connection_hdl hdl, message_ptr msg) {
     std::cout << "on_message called with hdl: " << hdl.lock().get()
               << " and message: " << msg->get_payload()
               << std::endl;
@@ -30,7 +28,7 @@ void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
 
     try {
         s->send(hdl, msg->get_payload(), msg->get_opcode());
-    } catch (const websocketpp::lib::error_code& e) {
+    } catch (const websocketpp::lib::error_code &e) {
         std::cout << "Echo failed because: " << e
                   << "(" << e.message() << ")" << std::endl;
     }
@@ -45,21 +43,21 @@ int main() {
         echo_server.set_access_channels(websocketpp::log::alevel::all);
         echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
 
-        // Initialize Asio
+        std::cout << "Initializing ASIO..." << std::endl;
         echo_server.init_asio();
 
-        // Register our message handler
-        echo_server.set_message_handler(bind(&on_message,&echo_server,::_1,::_2));
-
-        // Listen on port 9002
+        echo_server.set_message_handler(bind(&on_message, &echo_server, ::_1, ::_2));
         echo_server.listen(9002);
 
-        // Start the server accept loop
         echo_server.start_accept();
 
-        // Start the ASIO io_service run loop
+        std::cout << "Running ..." << std::endl;
+
         echo_server.run();
-    } catch (websocketpp::exception const & e) {
+
+        std::cout << "Exit." << std::endl;
+
+    } catch (websocketpp::exception const &e) {
         std::cout << e.what() << std::endl;
     } catch (...) {
         std::cout << "other exception" << std::endl;
